0d1fb1bacf22009bf979ebb95bec8c70ac952893
Ala Antabli <ala.antabli@etudiant-enit.utm.tn>
2022-01-27 02:23:38 -0500

    init_commit3

--- gitus.cpp
+++ gitus.cpp
@@ -5,11 +5,42 @@
 #include <init.h>
 #include <dummy.h>
 
-int main()
+
+void Help()
 {
-	TestDeTrucs();
-    std::cout << GetAnswerToLifeUniverseAndEverything() << std::endl;
-    return 0;
+	std::cout << "Guide d'utilisation: gitus <commande> [<arguments>]" << std::endl
+		<< "Voici les différentes commande gitus utilisées dans ce projet :" << std::endl
+		<< "init: Création d'une répertoire Git vide " << std::endl
+		<< "add: Ajout du contenu d'un fichier dans <index>" << std::endl
+		<< "commit:	Enregistre les modifications dans la répertoire" << std::endl;
+}
+
+
+std::vector<std::string> argvString;
+
+int main(int argc, char* argv[])
+{
+	// Enregistrement des arguments sous forme d'une chaine de caractères
+	argvString = std::vector<std::string>(argv, argv + argc);
+
+	// Au cas de demande d'aide ou de mal entrer les arguments
+	if (argc < 2 || argvString[1] == "--help")
+		Help();
+
+	// init
+	else if (argvString[1] == "init")
+	{
+		if (argc == 2)
+			Init();
+		else
+			std::cout << "usage: gitus init" << std::endl;
+	}
+
+	// Not a valid command
+	else
+		Help();
+
+    return 42;
 }
 
 
