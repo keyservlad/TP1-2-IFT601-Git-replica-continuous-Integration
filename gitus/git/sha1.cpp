f87f20b6001a2258990764eeb23b1e745c678dd4
Ala Antabli <ala.antabli@etudiant-enit.utm.tn>
2022-02-02 01:39:00 -0500

    Modification8

--- gitus/git/sha1.cpp
+++ gitus/git/sha1.cpp
@@ -11,28 +11,19 @@ using namespace boost::uuids::detail;
 
 string Sha1Generator(string path)
 {
-	ifstream file(path);
-
-	// lecture automatique du contenu et insertion dans une string
-	string content{ istreambuf_iterator<char>(file),
-					istreambuf_iterator<char>() };
-
-
 	sha1 sha;
-	sha.process_bytes(content.c_str(), content.length());
+	sha.process_bytes(path.c_str(), path.length());
 
 	unsigned int hash[5];
 	sha.get_digest(hash); // get 5 uint (32 bits -- that can be converted into 8 hex characters) that, combined, maked for the 40 characters
 	// This is done by just transforming the uints into hexadecimal values // 
 
 	stringstream stream;
-
-	string result;
 	for (int i = 0; i < 5; ++i)
 	{
 		stream << hex << hash[i] << setfill('0') << setw(8);
 	}
-	result += stream.str();
+	string result = stream.str();
 	return result;
 }
 
