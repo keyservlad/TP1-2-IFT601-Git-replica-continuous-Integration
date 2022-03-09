afed05c09be2160185fa6f771ca2d2c659f3ebe8
Arnaud Guilhamat <guia2612@usherbrooke.ca>
2022-03-08 23:40:45 -0500

    writing tests

--- BuildUs/tests/unittests.cpp
+++ BuildUs/tests/unittests.cpp
@@ -5,69 +5,36 @@
 #include <boost/process.hpp>
 #include <buildus.h>
 
-TEST_CASE("compile_build_and_clean")
+TEST_CASE("checkBuildusAndClean")
 {
     boost::system::error_code code;
 
-    // Get paths
-    const auto originalPath = boost::filesystem::current_path();
-    auto path = originalPath;
-    const auto intermediatePath = path.append("intermediate");
-    path = intermediatePath;
-    const auto mainPath_o = path.append("main.o");
-    path = intermediatePath;
-    auto helloPatho = path.append("helloworld.o");
-    path = originalPath;
-    const auto helloPathApp = path.append("tests").append("testsUtils").append("helloworld");
+    const auto currentPath = boost::filesystem::current_path();
+    const auto intermediatePath = currentPath / "intermediate";
+    const auto mainPatho = intermediatePath / "main.o";
+    const auto helloPatho = intermediatePath / "helloworld.o";
+    const auto helloPathApp = currentPath / "tests" / "testsUtils" / "helloworld";
 
-    // Execute BuildUs
+    // we execute Buildus and check that everything worked just fine
     REQUIRE(boost::process::system("./BuildUs tests/testsUtils/config.buildus tests/testsUtils") == 0);
 
-    // Check if directory and file creation was a success
-    REQUIRE(boost::filesystem::exists(mainPath_o, code));
+    // we check that teh files and directory were created
+    REQUIRE(boost::filesystem::exists(mainPatho, code));
     REQUIRE(boost::filesystem::exists(helloPatho, code));
     REQUIRE(boost::filesystem::exists(helloPathApp, code));
  
-    // Execute the created app
+    // Execute the created hello world app
     REQUIRE(boost::process::system("./tests/testsUtils/helloworld") == 0);
 
-    // Clean intermediate directory
+    // execute clean and check that intermediate dir is gone
     REQUIRE(boost::process::system("./BuildUs clean") == 0);
     REQUIRE(!boost::filesystem::exists(intermediatePath, code));
 }
 
-TEST_CASE("config_inexistent_config")
-{
-    // Execute BuildUs, should fail
-    REQUIRE(boost::process::system("./BuildUs invalid/folder/inexistent.config tests/testsUtils") == 1);
-}
-
-TEST_CASE("config_invalid_config")
-{
-    // Execute BuildUs, should fail (config file does not end with .buildus)
-    REQUIRE(boost::process::system("./BuildUs tests/testsUtils/config.invalid tests/testsUtils") == 1);
-}
-
-TEST_CASE("config_invalid_projectname")
-{
-    // Execute BuildUs, should fail (no project name in the config file)
-    REQUIRE(boost::process::system("./BuildUs tests/testsUtils/badconfig1.buildus tests/testsUtils") == 1);
-}
-
-TEST_CASE("compile_missing_file")
-{
-    // Execute BuildUs, should fail (trying to compile a missing file)
-    REQUIRE(boost::process::system("./BuildUs tests/testsUtils/badconfig2.buildus tests/testsUtils") == 1);
-}
-
-TEST_CASE("clean_no_intermediate_directory")
+TEST_CASE("cleanWithNoIntermediateDirectory")
 {
     boost::system::error_code code;
-
-    // Get paths
-    const auto originalPath = boost::filesystem::current_path();
-    auto path = originalPath;
-    const auto intermediatePath = path.append("intermediate");
+    const auto intermediatePath = boost::filesystem::current_path() / "intermediate";
 
     // First clean to make sure there is no intermediate folder
     clean();
@@ -76,3 +43,35 @@ TEST_CASE("clean_no_intermediate_directory")
     // Clean, should fail
     REQUIRE(clean() == 1);
 }
+
+// now we can check that the tests fail in case of bad config (we made few cases)
+TEST_CASE("inexistentConfig")
+{
+    // invalid config path or inexistant
+    REQUIRE(boost::process::system("./BuildUs tests/testsUtils/inexistent.config tests/testsUtils") == 1);
+}
+
+TEST_CASE("invalidConfig")
+{
+    // config file does not end with .buildus
+    REQUIRE(boost::process::system("./BuildUs tests/testsUtils/config.invalid tests/testsUtils") == 1);
+}
+
+TEST_CASE("invalidProjectname")
+{
+    // no project name in the config file
+    REQUIRE(boost::process::system("./BuildUs tests/testsUtils/badconfig1.buildus tests/testsUtils") == 1);
+}
+
+TEST_CASE("compileMissingFile")
+{
+    // trying to compile a missing file
+    REQUIRE(boost::process::system("./BuildUs tests/testsUtils/badconfig2.buildus tests/testsUtils") == 1);
+}
+
+TEST_CASE("noCompile")
+{
+    // no compile
+    REQUIRE(boost::process::system("./BuildUs tests/testsUtils/badconfig3.buildus tests/testsUtils") == 1);
+}
+
